4fd16e24d6429b072b9d4440e8fbae160c9c8452
Mahnoor <mahnooradil2001@gmail.com>
2026-04-18 08:45:39 +0100

    cricket-logo-Image hase been added

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BatWise Cricket - Home</title>
+    <link rel="stylesheet" href="css/horizontal.css">
+</head>
+
+<body>
+
+    <header>
+        <a href="index.php">
+            <img src="images/cricket-logo.png" width="75" height="75" alt="BatWise Cricket Logo">
+        </a>
+        <h1>BatWise Cricket</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about_us.php">About Us</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Member Login</a></li>
+        </ul>
+    </nav>
+
+    <main>
+
+    </main>
+
+    <?php require_once 'project_footer.php'; ?>
+
+</body>
+
+</html>
