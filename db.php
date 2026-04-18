f81170f35281e9457585f15ef75c9c3ac6f0a6b5
Mahnoor <mahnooradil2001@gmail.com>
2026-04-18 11:16:46 +0100

    Adding db file

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+/* Database connection settings */
+$host = "localhost";
+$dbname = "batwise_cricket";
+$username = "root";
+$password = "";
+
+/* Create a new MySQLi connection */
+$conn = new mysqli($host, $username, $password, $dbname);
+
+/* Check if the connection is successful */
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- hash_password.php
+++ hash_password.php
@@ -1,3 +1,6 @@
 <?php
 echo password_hash("admin123", PASSWORD_DEFAULT);
 ?>
+
+<!-- Hash password -->
+<!-- $2y$10$mpxGVFwnjMWmr7bRIUd/yOGs22RPsepeh6.rh9PazwxlSkWeAFzLq -->
