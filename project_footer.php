f94810761e8b65fc7388f17da1a653ed6a6c4745
Mahnoor <mahnooradil2001@gmail.com>
2026-04-20 12:28:45 +0100

    Added footer with css

---
+++ project_footer.php
@@ -0,0 +1,7 @@
+<footer class="site-footer">
+    <p>&copy; <?php echo date("Y"); ?> BatWise Cricket. All rights reserved.</p>
+    <p>
+        <a href="contact.php">Contact Us</a> |
+        <a href="about_us.php">About Us</a>
+    </p>
+</footer>
